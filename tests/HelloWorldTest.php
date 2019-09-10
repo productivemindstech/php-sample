efd82d27aadfb3f63db8e7535c5247d134d04595
surya <sendemail2surya@gmail.com>
2019-09-10 21:03:56 +0530

    e

--- tests/HelloWorldTest.php
+++ tests/HelloWorldTest.php
@@ -1,13 +1,13 @@
 <?php
 
 require_once 'vendor/autoload.php';
-/*define('SAUCE_HOST','pubinator:580f062b-af5a-4982-85dc-4619f66a2987@ondemand.saucelabs.com');*/
+define('SAUCE_HOST','pubinator:580f062b-af5a-4982-85dc-4619f66a2987@ondemand.saucelabs.com');
 
 class WebTest extends PHPUnit_Extensions_Selenium2TestCase {
 	
     protected $start_url = '';
 
-   /* public static $browsers = array(
+    public static $browsers = array(
         array(
             'browserName' => 'firefox',
             'host' => SAUCE_HOST,
@@ -17,12 +17,8 @@ class WebTest extends PHPUnit_Extensions_Selenium2TestCase {
                 'platform'=> 'Windows 10'
             )
         )
-    );*/
+    );
 
-	public static $browsers = RemoteWebDriver::create(
-    "https://pubinator:580f062b-af5a-4982-85dc-4619f66a2987@ondemand.saucelabs.com:443/wd/hub",
-    array("platform"=>"Windows 7", "browserName"=>"chrome", "version"=>"50")
-  );
 
     protected function setUp()
     {
@@ -33,7 +29,7 @@ class WebTest extends PHPUnit_Extensions_Selenium2TestCase {
 
     public function testTitle()
     {
-		$start_url = 'hello.html';
+		$start_url = 'http://localhost:8080/hello.html';
 		try {
         $this->url($start_url);
 		}catch(Exception $e) {
@@ -45,47 +41,3 @@ class WebTest extends PHPUnit_Extensions_Selenium2TestCase {
     }
 }
 ?>
-
-
-<?php /*
-require_once('vendor/autoload.php');
-use Facebook\WebDriver\Remote\RemoteWebDriver;
-use Facebook\WebDriver\WebDriverBy;
-
-
-
-class HelloWorldTest extends PHPUnit_Framework_TestCase {
-    public function testGreeting() {
-        $greeting = "Hello World";
-        $requiredGreeting = "Hello World";
-
-        $this->assertEquals($greeting, $requiredGreeting);
-    }
-
-    public function testSeleniumSauce() {
-//        $caps = Selenium::WebDriver::Remote::Capabilities.firefox([
-//          'tunnel-identifier' => env('TRAVIS_JOB_NUMBER')
-//        ]);
-//        $driver = Selenium::WebDriver.for(:remote, json_encode('{
-//                    url: "http://username:access_key@ondemand.saucelabs.com/wd/hub",
-//          desired_capabilities: "' . $caps . '"
-//        }'));
-
-//        print_r($caps, true);
-
-
-        $web_driver = Remote\RemoteWebDriver::create(
-            "https://pubinator:580f062b-af5a-4982-85dc-4619f66a2987@ondemand.saucelabs.com:443/wd/hub",
-            array("platform"=>"Windows 7", "browserName"=>"chrome", "version"=>"40")
-        );
-        $web_driver->get("https://saucelabs.com/test/guinea-pig");
-
-        //Test actions here...
-
-
-        $web_driver->quit();
-
-
-        $this->assertEquals(false, true);
-    }
-} */
