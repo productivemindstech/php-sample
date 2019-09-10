6555577c0b89907b51a5173495e2ec31a16ead9f
surya <sendemail2surya@gmail.com>
2019-09-10 21:17:59 +0530

    r

--- tests/HelloWorldTest.php
+++ tests/HelloWorldTest.php
@@ -1,43 +1,20 @@
+
 <?php
-
-require_once 'vendor/autoload.php';
-define('SAUCE_HOST','pubinator:580f062b-af5a-4982-85dc-4619f66a2987@ondemand.saucelabs.com');
-
-class WebTest extends PHPUnit_Extensions_Selenium2TestCase {
-	
-    protected $start_url = '';
-
-    public static $browsers = array(
-        array(
-            'browserName' => 'firefox',
-            'host' => SAUCE_HOST,
-            'port' => 80,
-            'desiredCapabilities' => array(
-                'version' => '69.0',
-                'platform'=> 'Windows 10'
-            )
-        )
-    );
-
-
-    protected function setUp()
-    {
-		$html_path = getcwd().'/';
-		$base_url = 'file:///'.$html_path;
-        $this->setBrowserUrl('http://localhost:8080/');
-    }
-
-    public function testTitle()
-    {
-		$start_url = 'http://localhost:8080/hello.html';
-		try {
-        $this->url($start_url);
-		}catch(Exception $e) {
-		echo 'Message: ' .$e->getMessage();
-		}
-		$currentURL = $this->url();
-		echo $currentURL;
-        $this->assertContains("PHP Hello World Test", $this->title());		
-    }
-}
+  // Setup: $ php composer.phar require facebook/webdriver
+ 
+  require_once('vendor/autoload.php');
+  use Facebook\WebDriver\Remote\RemoteWebDriver;
+  use Facebook\WebDriver\WebDriverBy;
+ 
+  $web_driver = RemoteWebDriver::create(
+    "https://pubinator:580f062b-af5a-4982-85dc-4619f66a2987@ondemand.saucelabs.com:443/wd/hub",
+    array("platform"=>"Windows 7", "browserName"=>"chrome", "version"=>"40")
+  );
+  $web_driver->get("https://saucelabs.com/test/guinea-pig");
+ 
+  /*
+    Test actions here...
+  */
+ 
+  $web_driver->quit();
 ?>
