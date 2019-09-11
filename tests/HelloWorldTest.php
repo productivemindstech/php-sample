0627ee64eed77015b9e1871499625c89d1a2f406
surya <sendemail2surya@gmail.com>
2019-09-11 21:22:43 +0530

    qq

--- tests/HelloWorldTest.php
+++ tests/HelloWorldTest.php
@@ -7,7 +7,7 @@
   use Facebook\WebDriver\WebDriverBy;
  
   $web_driver = RemoteWebDriver::create(
-    "https://pubinator:580f062b-af5a-4982-85dc-4619f66a2987@ondemand.saucelabs.com:443/wd/hub",
+    "http://pubinator:580f062b-af5a-4982-85dc-4619f66a2987@localhost:4445/wd/hub",
     array("platform"=>"Windows 7", "browserName"=>"chrome", "version"=>"60")
   );
   $web_driver->get("http://127.0.0.1:3000/hello.html");
