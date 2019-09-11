d5eaa0cd17361d7ce3b3df4f47e796af1c1bb530
surya <sendemail2surya@gmail.com>
2019-09-11 23:38:11 +0530

    d

--- before_script.sh
+++ before_script.sh
@@ -3,6 +3,7 @@ echo "server starting..."
 pwd 
 python -m SimpleHTTPServer 3000 > /dev/null 2>&1 &
 sleep 5
+cd ..
 echo "Starting suace connect"
 bin/sc -u pubinator -k 580f062b-af5a-4982-85dc-4619f66a2987
 sleep 35
